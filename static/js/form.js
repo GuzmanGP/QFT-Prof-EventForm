f98f0c81c9dcec7bda49e67b39efcaa7b73774cb
guzmangomezp1 <36497383-guzmangomezp1@users.noreply.replit.com>
2024-11-10 08:50:01 +0000

    Agent query: Are there any JavaScript console errors and is the form validation working correctly?

--- static/js/form.js
+++ static/js/form.js
@@ -1,6 +1,6 @@
 import { initializeForm } from './init.js';
 import { validateForm } from './validation.js';
-import { addQuestion, removeQuestion } from './question.js';
+import { addQuestion } from './question.js';
 import { updateMetadataFields } from './metadataFields.js';
 import { showAlert } from './utils.js';
 
